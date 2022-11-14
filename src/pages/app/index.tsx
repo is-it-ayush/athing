cbd1f1b3c35a5c6deecae8ba521c533a1c516705
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-11-14 14:29:25 +0530

    made it a component

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -11,6 +11,7 @@ import { useAtom } from 'jotai';
 
 // Atoms
 import { userInfo } from '@utils/store';
+import { FullLoad } from '@components/ui/FullLoad';
 
 export const App: NextPage = () => {
 	const userInfoResponse = trpc.user.me.useQuery(void 0, {
@@ -38,16 +39,7 @@ export const App: NextPage = () => {
 
 	return (
 		<AnimatePresence>
-			{showLoading === true ? (
-				<motion.div
-					className="fixed top-0 left-0 flex h-screen w-screen flex-row items-center justify-center bg-white "
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-					transition={{ duration: 0.5 }}>
-					<div className="flex h-6 w-6 animate-spin rounded-full border-b-2 border-black"></div>
-				</motion.div>
-			) : null}
+			{showLoading === true ? <FullLoad /> : null}
 			<Navbar key="navigation" pageController={setShowPage} menuController={[showMenu, setShowMenu]} />
 			<motion.div
 				key="content"
