c15f81bf9431984d994d70bb02e4fba3358e9053
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-11-14 13:12:09 +0530

    added .env.local

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,13 +13,13 @@ const Home: NextPage = () => {
 					exit={{ opacity: 0 }}
 					transition={{ duration: 0.5 }}>
 					<div className="flex flex-col">
-						<h1 className="flex text-center">I'm still working on it, btw you're amazing :{')'}</h1>
+						<h1 className="flex text-center">I&apos;m still working on it, btw you&apos;re amazing :)</h1>
 					</div>
 					<div className="fixed bottom-5 left-0 flex w-full items-center justify-center">
 						<h1 className="flex">
 							Twitter:{' '}
 							<Link href="https://twitter.com/is_it_ayush" target="_blank">
-								<h1 className="text-blue-500 mx-2">@is_it_ayush</h1>
+								<h1 className="mx-2 text-blue-500">@is_it_ayush</h1>
 							</Link>
 						</h1>
 					</div>
