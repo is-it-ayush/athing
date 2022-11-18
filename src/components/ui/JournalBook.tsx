57e9c0f1e5d94affd976d1abc26d0cb6c7fd815e
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-11-18 17:29:50 +0530

    seperated jounral box into jounral book, and added jounral picker

---
+++ src/components/ui/JournalBook.tsx
@@ -0,0 +1,71 @@
+import { type Journal } from '@prisma/client';
+import { JournalBookProps } from '@utils/client.typing';
+import { formatDate } from '@utils/client.util';
+import {
+	selectedJournalAtom,
+	showJournalIndexModalAtom,
+	showJournalPickerAtom,
+	showToastAtom,
+	toastIntentAtom,
+	toastMessageAtom,
+} from '@utils/store';
+import { motion } from 'framer-motion';
+import { useAtom } from 'jotai';
+
+export const JournalBook = ({ journal, type }: JournalBookProps) => {
+	const [, setSelectedJournal] = useAtom(selectedJournalAtom);
+	const [, setShowJournalIndexModal] = useAtom(showJournalIndexModalAtom);
+
+	const [, setShowJournalPickerModal] = useAtom(showJournalPickerAtom);
+
+	// Toast
+	const [, setDisplayToast] = useAtom(showToastAtom);
+	const [, setToastIntent] = useAtom(toastIntentAtom);
+	const [, setToastMessage] = useAtom(toastMessageAtom);
+
+	return (
+		<motion.li
+			key={journal.id}
+			onClick={() => {
+				setSelectedJournal(journal);
+				if (type === 'view') {
+					setShowJournalIndexModal(true);
+				} else if (type === 'select') {
+					// Scroll to top smoothly
+					window.scrollTo({
+						top: 0,
+						behavior: 'smooth',
+					});
+					setShowJournalPickerModal(false);
+					setDisplayToast(true);
+					setToastIntent('info');
+					setToastMessage(journal.title.substring(0, 20) + '... has been selected.');
+				}
+			}}
+			className={
+				`m-5 flex h-[300px] cursor-pointer flex-col justify-evenly border-2 bg-white bg-overlapcrc-pattern p-5 transition-all hover:border-black ` +
+				(type === 'select' ? 'w-[200px]' : 'min-w-[200px]')
+			}
+			layout
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 0.3 }}>
+			<div className="flex flex-col">
+				<div className="my-3 flex flex-col">
+					<h1 className="flex h-[120px] items-center break-words text-xl font-bold text-black">
+						{journal.title.length > 25 ? `${journal.title.substring(0, 25)}...` : journal.title}
+					</h1>
+					<div className="mt-2 w-fit">
+						{journal.isPublic ? (
+							<h1 className=" bg-pink-600 p-2 text-black">Public</h1>
+						) : (
+							<h1 className="bg-black p-2 text-white">Private</h1>
+						)}
+					</div>
+				</div>
+				<h4 className=" text-sm text-gray-600">{formatDate(journal.createdAt)}</h4>
+			</div>
+		</motion.li>
+	);
+};
---
+++ src/components/ui/JournalPicker.tsx
@@ -0,0 +1,77 @@
+import { Journal } from '@prisma/client';
+import { showJournalPickerAtom, userInfo } from '@utils/store';
+import { trpc } from '@utils/trpc';
+import { motion } from 'framer-motion';
+import { useAtom } from 'jotai';
+import { useEffect, useState } from 'react';
+import { IoClose } from 'react-icons/io5';
+import { Button } from './Button';
+import { JournalBook } from './JournalBook';
+
+const JournalPickerAnimations = {
+	hidden: {
+		opacity: 0,
+	},
+	visible: {
+		opacity: 1,
+	},
+};
+
+export const JournalPicker = () => {
+	// Atoms
+	const [, setShowJournalPickerModal] = useAtom(showJournalPickerAtom);
+	const [journals, setJournals] = useState<Journal[]>([]);
+	const [user] = useAtom(userInfo);
+
+	// tRPC
+	const journalsQuery = trpc.journals.getJournalsByUserId.useQuery(
+		{
+			id: user.id,
+		},
+		{
+			onError(err) {
+				setJournals([]);
+			},
+		},
+	);
+
+	useEffect(() => {
+		if (journalsQuery.data) {
+			setJournals(journalsQuery.data);
+		}
+	}, [journalsQuery.data]);
+
+	return (
+		<motion.div
+			className="absolute top-0 left-0 flex min-h-screen w-screen items-center justify-center bg-white"
+			initial={JournalPickerAnimations.hidden}
+			animate={JournalPickerAnimations.visible}
+			exit={JournalPickerAnimations.hidden}
+			transition={{
+				duration: 0.5,
+			}}>
+			<div className="absolute top-5 right-5">
+				<Button
+					type="button"
+					onClick={() => {
+						setShowJournalPickerModal(false);
+					}}
+					width="fit"
+					styles="opposite">
+					<IoClose className="h-6 w-6" />
+				</Button>
+			</div>
+			<div className="flex flex-col flex-wrap p-10 lg:flex-row">
+				{journals?.length > 0 ? (
+					journals.map((journal, i) => {
+						return <JournalBook type="select" journal={journal} key={journal.id} />;
+					})
+				) : (
+					<div className="flex flex-col">
+						<h1 className="text-center text-2xl font-bold">You have no journals! Create One First!</h1>
+					</div>
+				)}
+			</div>
+		</motion.div>
+	);
+};
