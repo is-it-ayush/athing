7ba202817fde9778ac65a99ff3bb6d629c13eee5
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-11-20 18:51:04 +0530

    improved feedback ui

--- src/components/ui/FeedbackModal.tsx
+++ src/components/ui/FeedbackModal.tsx
@@ -3,7 +3,8 @@ import { trpc } from '@utils/trpc';
 import { motion } from 'framer-motion';
 import { useAtom } from 'jotai';
 import { useState } from 'react';
-import { BiUserVoice } from 'react-icons/bi';
+import { BiMailSend, BiUserVoice } from 'react-icons/bi';
+import { GrFormClose } from 'react-icons/gr';
 import TextareaAutosize from 'react-textarea-autosize';
 import { Button } from './Button';
 
@@ -56,17 +57,17 @@ export const FeedbackModal = () => {
 
 	return (
 		<motion.div
-			className="fixed top-0 left-0 flex min-h-screen w-screen items-center justify-center font-spacemono"
+			className="no-select fixed top-0 left-0 flex min-h-screen w-screen items-center justify-center font-spacemono"
 			initial={FeedbackModalAnimations.hidden}
 			animate={FeedbackModalAnimations.visible}
 			exit={FeedbackModalAnimations.hidden}
 			transition={FeedbackModalAnimations.transition}>
-			<div className="flex min-h-[400px] w-[300px] flex-col items-center justify-center border-2 border-gray-300 bg-white">
+			<div className="flex min-h-[350px] w-[300px] flex-col items-center justify-center border-none border-gray-300 bg-white">
 				<div className="mb-2 flex">
 					<BiUserVoice className="h-10 w-10" />
 				</div>
 				<div className="my-2 flex flex-col justify-center">
-					<h1 className="text-center text-2xl font-semibold">Hear my voice</h1>
+					<h1 className="text-center text-2xl font-semibold">Hear my Voice</h1>
 					<TextareaAutosize
 						value={text}
 						onChange={(e) => {
@@ -79,23 +80,26 @@ export const FeedbackModal = () => {
 							}
 						}}
 						maxRows={6}
-						className="my-2 min-h-fit w-full resize-none border-none p-2 text-center focus:border-white focus:ring-0"
+						className="my-2 min-h-fit w-full resize-none border-none p-2 text-start placeholder:text-center focus:border-white focus:ring-0"
 						placeholder="Want a cool new feature? Found a bug? Let me know!"
 					/>
 				</div>
-				<div className="my-2 flex flex-row gap-5">
-					<Button
-						type="button"
-						width="fit"
-						styles="opposite"
+				<div className="my-2 flex flex-row items-center justify-center gap-5">
+					<button
+						className="flex items-center justify-center rounded-full border-2 p-3 duration-200 hover:border-gray-400"
 						onClick={() => {
 							setShowFeedback(false);
 						}}>
-						Cancel
-					</Button>
-					<Button type="button" width="fit" styles="exotic" onClick={handleFeedbackSubmit}>
-						Submit
-					</Button>
+						<GrFormClose className="h-6 w-6" />
+					</button>
+
+					<button
+						className="flex items-center justify-center rounded-full border-2 p-3 duration-200 hover:border-gray-400"
+						onClick={() => {
+							handleFeedbackSubmit();
+						}}>
+						<BiMailSend className="h-6 w-6" />
+					</button>
 				</div>
 			</div>
 		</motion.div>
