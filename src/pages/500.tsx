20466b13eed123a0018b383ba9d899506ef2e7dd
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-11-21 18:10:45 +0530

    500 build fix

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -21,7 +21,7 @@ export default function Error() {
 			transition={NotFoundAnimations.transition}>
 			<div className="flex flex-col items-center justify-center p-5 text-center">
 				<h1 className="text-9xl font-bold text-red-600">500</h1>
-				<h2 className="my-3 text-3xl">I know, I write bad code. You didn't have to crash it. (Working on it. :)</h2>
+				<h2 className="my-3 text-3xl">I know, I write bad code. You didn&apos;t have to crash it. (Working on it. :)</h2>
 				<Button
 					flex="row"
 					width="fit"
