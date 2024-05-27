0add9e6ee3c41dadf882a1432fd3e502d6f5f8dc
Ayush Gupta <ayush.keshav2004@gmail.com>
2024-05-27 16:41:24 +0530

    patch: i accidently deleted the database...

--- prisma/migrations/20221123032839_postgres_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Feedback" DROP CONSTRAINT "Feedback_authorId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
